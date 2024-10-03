97e5003e9f9c81e6e31dd228b7431b5c9c3a3e8c
Mohamed Amine Fhal <fhal.mohamed.amine@gmail.com>
2024-10-03 07:19:44 +0100

    create new todos

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,11 @@ const app = express();
 const fs = require("node:fs")
 const PATH = './todos.json'
 
+// This allow us to read the body from the request
+app.use(express.json())
 
+
+// Get all the todos from the todos.json file
 const load_todos = () => {
     // check if the todos.json exists
     if(fs.existsSync(PATH)) {
@@ -27,19 +31,36 @@ const load_todos = () => {
 }
 
 // write a function that create and store the todo in todos.json file
+const save_new_todos = (todos) => {
+    // [ {}, {}, {} ] => "[ {}, {}, {} ]"
+    const oldTodos = load_todos()
+    const allTodos = oldTodos.concat(todos)
+    fs.writeFileSync(PATH, JSON.stringify(allTodos))
+}
 
-
+// GET req
 app.get("/", (req, res) => {
 	// http:localhost:8888/
-	res.write("Hello world, this is from Express framework");
+	res.write("Hello world, this is from express!!");
 	res.end();
 });
 
+// GET req /todos
 app.get("/todos", (req, res) => {
     const todos = load_todos() // []
     return res.json(todos)
 });
 
+
+// POST req /todos
+app.post("/todos", (req, res) => {
+    console.log(req.body)
+    save_new_todos(req.body)
+    return res.json("Todos are saved!!")
+})
+
+
+// Start listening to the PORT 8888
 app.listen(8888, () => {
 	console.log("The server is running 🚀");
 });
